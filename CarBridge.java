053087f536f6f09716cf686fbea342e20998aaca
eddielife2k <44607352+eddielife2k@users.noreply.github.com>
2019-02-06 13:33:37 -0500

    Created CarBridge.java

---
+++ CarBridge.java
@@ -0,0 +1,24 @@
+public abstract class CarBridge<E> implements Bridge<E>
+{
+    private E length;
+    private String materials;
+    private E weightLimit;
+    public CarBridge() {
+        materials = "";
+    }
+    public void materials(String m) {
+    }
+    public String returnMaterials() {
+        return "";
+    }
+    public void length(E length) {
+    }
+    public E returnLength() {
+        return length;
+    }
+    public void weightLimit(E lim) {
+    }
+    public E returnWeightLimit() {
+        return weightLimit;
+    }
+}
