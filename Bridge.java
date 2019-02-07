3c2b6377b9e2db8fccadd22ad84fe524f3db374b
eddielife2k <44607352+eddielife2k@users.noreply.github.com>
2019-02-07 18:09:06 -0500

    Update Bridge.java

--- Bridge.java
+++ Bridge.java
@@ -1,5 +1,5 @@
 public interface Bridge<E> {
-    public void materials(String m);
+    public void materials(String[] m);
     public String returnMaterials();
     public void length(E length);
     public E returnLength();
