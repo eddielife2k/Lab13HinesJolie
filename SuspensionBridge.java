3afb533cddfd296eb9196c4855108e50460b7140
spencerjolie <44411669+spencerjolie@users.noreply.github.com>
2019-02-11 12:38:41 -0500

    Update SuspensionBridge.java

--- SuspensionBridge.java
+++ SuspensionBridge.java
@@ -1,10 +1,13 @@
 public class SuspensionBridge<E>extends CarBridge<E>
 {
-   public SuspensionBridge(String[] m, E l, E wL, E sL, int nL)
+   private double temp;
+   double[] tensions;
+  public SuspensionBridge(String[] m, E l, E wL, E sL, int nL, double[] tensions)
    {
        super(m, l, wL, sL, nL);
+       this.tensions = tensions; 
    }
-   public void materials(String m){}
+  public void materials(String m){}
   public void length(E l) {}
   public void weightLimit(E lim) {}
   public void speedLimit(E lim) {}
@@ -13,6 +16,18 @@ public class SuspensionBridge<E>extends CarBridge<E>
     }
   public void archLength(E c){
     }
- public void cantileverLength(E length){}
-  public void tensions(double[] tensions) {}
+  public void cantileverLength(E length){}
+  public void tensions(double[] tensions) {
+    for(int k = 0; k < tensions.length; k++){
+    temp = tensions[k];
+    tensions[k] = temp;
+    }
+  }
+  public String returnTensions(){
+      String temp = "";
+      for(double str: tensions){
+          temp += str + ", ";
+        }
+        return temp;
+    }
 }
