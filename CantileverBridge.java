71d9d6eb2194ce18b4855864712a8b5d18dc8e17
spencerjolie <44411669+spencerjolie@users.noreply.github.com>
2019-02-11 12:25:39 -0500

    Update CantileverBridge.java

--- CantileverBridge.java
+++ CantileverBridge.java
@@ -1,10 +1,12 @@
 public class CantileverBridge<E>extends CarBridge<E>
 {
+    private E cantileverLength;
   public CantileverBridge(String[] m, E l, E wL, E sL, int nL)
   {
       super(m, l, wL, sL, nL);
+      cantileverLength = cL;
   }
-  public void materials(String[] m){}
+  public void materials(String m){}
   public void length(E l) {}
   public void weightLimit(E lim) {}
   public void speedLimit(E lim) {}
@@ -13,6 +15,8 @@ public class CantileverBridge<E>extends CarBridge<E>
     }
   public void archLength(E c){
     }
- public void cantileverLength(E length){}
+ public void cantileverLength(E length){
+    cantileverLength = length;
+    }
   public void tensions(double[] tensions) {}
 }
