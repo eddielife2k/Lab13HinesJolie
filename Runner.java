1ed4fd53f64d7683d8b12c7fe8e27c35391ac3f3
eddielife2k <44607352+eddielife2k@users.noreply.github.com>
2019-02-13 08:38:56 -0500

    Update Runner.java

--- Runner.java
+++ Runner.java
@@ -6,5 +6,11 @@ public class Runner
         CarBridge<Integer> b1 = new ArchBridge<Integer>( materials, 500, 2500, 40, 2, 400, 128 );
         b1.speedLimit(45);
         System.out.println(b1);
+        CarBridge<String> b2 = new CantileverBridge<String>( materials, "Very Long", "that their can't be trucks", 
+        "no faster than highway limit of 65", 4, "50 feet long" );
+        System.out.println(b2);
+        double[] tensions = {55.64, 55.33};
+        CarBridge<Integer> b3 = new SuspensionBridge<Integer>( materials, 500, 2500, 40, 2, tensions );
+        System.out.println(b3);
    }
 }
