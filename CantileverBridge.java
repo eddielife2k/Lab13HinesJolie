01f9296547dcf9a5de59463d997edc6c7ee56b12
spencerjolie <44411669+spencerjolie@users.noreply.github.com>
2019-02-13 08:24:12 -0500

    Update CantileverBridge.java

--- CantileverBridge.java
+++ CantileverBridge.java
@@ -1,21 +1,29 @@
 public class CantileverBridge<E>extends CarBridge<E>
 {
     private E cantileverLength;
-  public CantileverBridge(String[] m, E l, E wL, E sL, int nL, E cL)
+ public CantileverBridge(String[] m, E l, E wL, E sL, int nL, E cL)
   {
       super(m, l, wL, sL, nL);
       cantileverLength = cL;
   }
-  public void archRadius(E max){
+ public void archRadius(E max){
     }
-  public void archLength(E c){
+ public void archLength(E c){
     }
  public void cantileverLength(E length){
     cantileverLength = length;
     }
-    public E returnCantileverLength(){
+ public E returnCantileverLength(){
         return cantileverLength;
     }
-  public void tensions(double[] tensions) {}
+ public void tensions(double[] tensions) {}
+ public String toString(){
+     String temp = "";
+     temp += returnMaterials() + ".\n";
+     temp += "The weight limit is " + returnWeightLimit() + ".\n";
+     temp += "The length is " + returnLength() + ".\n";
+     temp += "The speed limit is " + returnSpeedLimit() + ".\n";
+     temp += "The number of lanes are " + returnNumLanes() + ".\n";
+     temp += "The cantilever length is " + returnCantileverLength() + ".\n";
+    }
 }
-
