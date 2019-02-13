9e06693bd3587c97270f8a42955d6e6e22ee162e
eddielife2k <44607352+eddielife2k@users.noreply.github.com>
2019-02-13 07:56:38 -0500

    Update Runner.java

--- Runner.java
+++ Runner.java
@@ -2,8 +2,10 @@
 public class Runner
 {
     public static void main(String[] args) {
-        String[] materials = {"Concrete", "Stone"};
-        Bridge<Integer> bl = new ArchBridge<Integer>( materials, 500, 2500, 40, 2, 400, 128 );
+        String[] materials = {"Concrete", "Stone", "Asphalt"};
+        CarBridge<Integer> bl = new ArchBridge<Integer>( materials, 500, 2500, 40, 2, 400, 128 );
+        bl.materials(materials);
+        System.out.println(bl.returnMaterials());
         bl.speedLimit(45);
    }
 }
